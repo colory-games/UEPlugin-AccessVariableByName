3103249f2033191c7c9080559f0758483fde5baf
nutti <nutti.metro@gmail.com>
2022-10-08 16:06:39 +0900

    Fix: Build error

--- samples/SampleProject/Source/SampleProject.Target.cs
+++ samples/SampleProject/Source/SampleProject.Target.cs
@@ -1,14 +1,21 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+/*!
+ * SampleProject
+ *
+ * Copyright (c) 2022 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class SampleProjectTarget : TargetRules
 {
-	public SampleProjectTarget( TargetInfo Target) : base(Target)
+	public SampleProjectTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "SampleProject" } );
+		ExtraModuleNames.AddRange(new string[]{"SampleProject"});
 	}
 }
--- samples/SampleProject/Source/SampleProjectEditor.Target.cs
+++ samples/SampleProject/Source/SampleProjectEditor.Target.cs
@@ -1,14 +1,21 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+/*!
+ * SampleProject
+ *
+ * Copyright (c) 2022 Colory Games
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class SampleProjectEditorTarget : TargetRules
 {
-	public SampleProjectEditorTarget( TargetInfo Target) : base(Target)
+	public SampleProjectEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "SampleProject" } );
+		ExtraModuleNames.AddRange(new string[]{"SampleProject"});
 	}
 }
