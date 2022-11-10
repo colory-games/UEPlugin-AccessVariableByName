f5efeb39db125a9fa82e46e511665a5eb32f5702
nutti <nutti.metro@gmail.com>
2022-11-10 20:22:51 +0900

    Fix: Linter error

--- AccessVariableByName/Source/VariableAccessFunctionLibrary/Public/VariableAccessFunctionLibraryUtils.h
+++ AccessVariableByName/Source/VariableAccessFunctionLibrary/Public/VariableAccessFunctionLibraryUtils.h
@@ -33,8 +33,8 @@ struct FVarDescription
 namespace FVariableAccessFunctionLibraryUtils
 {
 VARIABLEACCESSFUNCTIONLIBRARY_API FProperty* GetScriptStructProperty(UScriptStruct* ScriptStruct, FString VarName);
-VARIABLEACCESSFUNCTIONLIBRARY_API bool HandleTerminalProperty(const TArray<FVarDescription>& VarDescs, int32 VarDepth, UObject* OuterObject, FProperty* Dest,
-	void* DestAddr, FProperty* NewValue, void* NewValueAddr);
+VARIABLEACCESSFUNCTIONLIBRARY_API bool HandleTerminalProperty(const TArray<FVarDescription>& VarDescs, int32 VarDepth,
+	UObject* OuterObject, FProperty* Dest, void* DestAddr, FProperty* NewValue, void* NewValueAddr);
 VARIABLEACCESSFUNCTIONLIBRARY_API void SplitVarName(const FString& In, TArray<FString>* Out);
 VARIABLEACCESSFUNCTIONLIBRARY_API void AnalyzeVarNames(const TArray<FString>& VarNames, TArray<FVarDescription>* VarDescs);
 }	 // namespace FVariableAccessFunctionLibraryUtils
