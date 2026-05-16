8a8481c85d3ddffeb7cec8d6e8713b7073e54b76
nutti <nutti.metro@gmail.com>
2026-05-16 17:42:18 +0530

    Fix: Failed to parse the variable name with square brackets (#64)

--- AccessVariableByName/Source/VariableAccessFunctionLibrary/Private/VariableAccessFunctionLibraryUtils.cpp
+++ AccessVariableByName/Source/VariableAccessFunctionLibrary/Private/VariableAccessFunctionLibraryUtils.cpp
@@ -536,7 +536,7 @@ void AnalyzeVarNames(const TArray<FString>& VarNames, TArray<FVarDescription>* V
 	{
 		// String pattern.
 		{
-			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_][a-zA-Z0-9_ ]*)\\[\"(\\S+)\"\\]$");
+			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_\\[\\]][a-zA-Z0-9_ \\[\\]]*)\\[\"(\\S+)\"\\]$");
 			FRegexMatcher Matcher(Pattern, Var);
 			if (Matcher.FindNext())
 			{
@@ -553,7 +553,7 @@ void AnalyzeVarNames(const TArray<FString>& VarNames, TArray<FVarDescription>* V
 
 		// Integer pattern.
 		{
-			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_][a-zA-Z0-9_ ]*)\\[([0-9]+)\\]$");
+			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_\\[\\]][a-zA-Z0-9_ \\[\\]]*)\\[([0-9]+)\\]$");
 			FRegexMatcher Matcher(Pattern, Var);
 			if (Matcher.FindNext())
 			{
@@ -570,7 +570,7 @@ void AnalyzeVarNames(const TArray<FString>& VarNames, TArray<FVarDescription>* V
 
 		// None pattern.
 		{
-			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_][a-zA-Z0-9_ ]*)$");
+			FRegexPattern Pattern = FRegexPattern("^([a-zA-Z_\\[\\]][a-zA-Z0-9_ \\[\\]]*)$");
 			FRegexMatcher Matcher(Pattern, Var);
 			if (Matcher.FindNext())
 			{
